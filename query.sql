d0fdcac0d6e5952a0fc49383ee2a179a37219629
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2022-02-22 14:32:22 +0100

    Add: query 5

--- query.sql
+++ query.sql
@@ -1,4 +1,4 @@
--- 1)Selezionare tutti gli studenti nati nel 1990 (160)
+-- 1)Selezionare tutti gli studenti nati nel 1990
 SELECT * 
 FROM `students`
 WHERE YEAR(`date_of_birth`)=1990
@@ -12,3 +12,15 @@ WHERE `cfu` > 10
 SELECT * 
 FROM `students`
 WHERE `date_of_birth` <= "1992-02-22"
+
+--4)Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea
+SELECT * 
+FROM `courses`
+WHERE `period` = "I semestre"
+	AND `year` = 1;
+
+--5)Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020
+SELECT * 
+FROM `exams`
+WHERE `date` = "2020-06-20"
+	AND `hour` > TIME("14:00:00");
