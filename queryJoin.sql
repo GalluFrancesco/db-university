4a8624683e06e2a3400b7555f7f07d34d082a5ba
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2022-02-23 16:54:25 +0100

    Add: query 3 (join)

--- queryJoin.sql
+++ queryJoin.sql
@@ -5,9 +5,18 @@ JOIN `degrees`
 	ON `students`.`degree_id` = `degrees`.`id`
 WHERE `degrees`.`name` = 'Corso di Laurea in Economia'
 
---1)Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+--2)Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
 SELECT `degrees`.* 
 FROM  `degrees`
 JOIN `departments`
 	ON `departments`.`id` = `degrees`.`department_id`
 WHERE `departments`.`name` = 'Dipartimento di Neuroscienze'
+
+--3)Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+SELECT `courses`.*
+FROM `courses`
+JOIN `course_teacher`
+	ON `courses`.`id` = `course_teacher`.`course_id`
+JOIN `teachers`
+	ON `course_teacher`.`teacher_id` = `teachers`.`id`
+WHERE `course_teacher`.`teacher_id` = 44
