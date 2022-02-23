fb8110a5226afd46f46029f751c9128d7ffc6e0d
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2022-02-23 16:50:13 +0100

    Add: query 2 (join)

--- queryJoin.sql
+++ queryJoin.sql
@@ -4,3 +4,10 @@ FROM `students`
 JOIN `degrees`
 	ON `students`.`degree_id` = `degrees`.`id`
 WHERE `degrees`.`name` = 'Corso di Laurea in Economia'
+
+--1)Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+SELECT `degrees`.* 
+FROM  `degrees`
+JOIN `departments`
+	ON `departments`.`id` = `degrees`.`department_id`
+WHERE `departments`.`name` = 'Dipartimento di Neuroscienze'
